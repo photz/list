63154ffc284d6761eca1128b29ba9f55c5b63260
P. Hotz <p.hotz@posteo.net>
2017-09-14 21:50:58 +0200

    Add a Tree class

--- main.js
+++ main.js
@@ -1,5 +1,88 @@
 let my = {}
 
+my.Tree = function (tree) {
+  this.element_ = document.createElement('div')
+  this.element_.classList.add('tree')
+  let rendered = this.renderTree_(tree)
+  this.element_.appendChild(rendered)
+}
+my.Tree.prototype.getElement = function () {
+  return this.element_
+}
+my.Tree.prototype.renderTree_ = function(tree) {
+  const rootEl = this.createBranch_(tree.direction)
+
+  let current = null
+
+  const stack = new Array()
+
+  stack.push({
+    element: rootEl,
+    children: tree.children
+  })
+
+  while (0 < stack.length) {
+    current = stack.pop()
+
+    current.children.forEach((child) => {
+
+      let childEl = null
+
+      if (child.type === my.NodeType.BRANCH) {
+        childEl = this.createBranch_(child.direction)
+        stack.push({element: childEl, children: child.children})
+      }
+      else if (child.type === my.NodeType.LEAF) {
+        childEl = this.createLeaf_(child.content)
+      }
+      else {
+        throw new TypeError('unknown node type')
+      }
+
+      current
+        .element
+        .querySelector('.tree__content')
+        .appendChild(childEl)
+    })
+
+  }
+  return rootEl
+}
+my.Tree.prototype.createLeaf_ = function (content) {
+  let el = document.createElement('div')
+  el.classList.add('tree__leaf')
+  el.innerHTML = content
+  return el
+}
+my.Tree.prototype.createBranch_ = function (direction) {
+  let el = document.createElement('div')
+  el.classList.add('tree__branch')
+  let dirMod = ''
+  if (direction === my.Direction.ROW) {
+    dirMod = 'tree__branch--row'
+  }
+  else if (direction === my.Direction.COLUMN) {
+    dirMod = 'tree__branch--column'
+  }
+  else {
+    throw new Error('unknown value for direction')
+  }
+  el.classList.add(dirMod)
+  let handleEl = document.createElement('div')
+  handleEl.classList.add('tree__handle')
+  el.appendChild(handleEl)
+  let contentEl = document.createElement('div')
+  contentEl.classList.add('tree__content')
+  el.appendChild(contentEl)
+  return el
+}
+my.Tree.prototype.renderNode_ = function () {
+  
+}
+my.Tree.prototype.renderLeaf_ = function () {
+  
+}
+
 /**
  * @constructor
  */
@@ -97,6 +180,16 @@ my.List.prototype.handleMouseDownOnPlaceholder_ = function (ev) {
   this.dragged_ = null
 }
 
+my.NodeType = {
+  BRANCH: 'node',
+  LEAF: 'leaf'
+}
+
+my.Direction = {
+  ROW: 'row',
+  COLUMN: 'column'
+}
+
 window.addEventListener('load', () => {
   let container = document.querySelector('.container')
   let items = [
@@ -107,4 +200,72 @@ window.addEventListener('load', () => {
   ]
   let list = new my.List(items)
   container.appendChild(list.getElement())
+  const treeData = {
+    type: my.NodeType.BRANCH,
+    direction: my.Direction.COLUMN,
+    children: [
+      {
+        type: my.NodeType.LEAF,
+        content: 'OCaml'
+      },
+      {
+        type: my.NodeType.LEAF,
+        content: 'Smalltalk'
+      },
+      {
+        type: my.NodeType.LEAF,
+        content: 'Swift'
+      },
+      {
+        type: my.NodeType.LEAF,
+        content: 'Racket'
+      },
+      {
+        type: my.NodeType.LEAF,
+        content: 'Dart'
+      },
+      {
+        type: my.NodeType.BRANCH,
+        direction: my.Direction.ROW,
+        children: [
+          {
+            type: my.NodeType.LEAF,
+            content: 'Common LISP'
+          },
+          {
+            type: my.NodeType.LEAF,
+            content: 'Scheme'
+          },
+          {
+            type: my.NodeType.LEAF,
+            content: 'Python'
+          },
+          {
+            type: my.NodeType.BRANCH,
+            direction: my.Direction.COLUMN,
+            children: [
+              {
+                type: my.NodeType.LEAF,
+                content: 'COBOL'
+              },
+              {
+                type: my.NodeType.LEAF,
+                content: 'C++'
+              },
+              {
+                type: my.NodeType.LEAF,
+                content: 'Java'
+              },
+              {
+                type: my.NodeType.LEAF,
+                content: 'Elixir'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+  let tree = new my.Tree(treeData)
+  container.appendChild(tree.getElement())
 })
